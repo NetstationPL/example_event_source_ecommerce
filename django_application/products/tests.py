e7968f7c07e3057de27817ebe7882e6b25a66290
climbus <climbus@gmail.com>
2022-02-21 20:18:13 +0100

    products index test case

--- django_application/products/tests.py
+++ django_application/products/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from products.models import Product
+
+
+class TestProducts(TestCase):
+    def setUp(self) -> None:
+        Product.objects.create(name="Test Product 1", price=10.00, vat_rate_code=10)
+        Product.objects.create(name="Test Product 2", price=20.00, vat_rate_code=20)
+        return super().setUp()
+
+    def test_products_page(self):
+        response = self.client.get("/products/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products.html")
