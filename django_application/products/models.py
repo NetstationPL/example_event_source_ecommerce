e4e3e9ffb92c5391e6420fea82a4379b5810b8f2
climbus <climbus@gmail.com>
2022-02-21 20:01:49 +0100

    product model

--- django_application/products/models.py
+++ django_application/products/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    VAT_RATE_CHOICES = (
+        (0, '0%'),
+        (5, '5%'),
+        (8, '8%'),
+        (23, '23%'),
+    )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    stock_level = models.IntegerField(default=0)
+    registered_at = models.DateTimeField(auto_now_add=True)
+    vat_rate_code = models.IntegerField(choices=VAT_RATE_CHOICES)
+
+    def __str__(self):
+        return self.name
