a46b984535575aeb07fb580f98d59799e8f6e48f
climbus <climbus@gmail.com>
2022-02-22 12:47:12 +0100

    product detail page

--- django_application/products/tests.py
+++ django_application/products/tests.py
@@ -5,12 +5,14 @@ from products.models import Product
 
 
 class TestProducts(TestCase):
-    def setUp(self) -> None:
-        Product.objects.create(name="Test Product 1", price=10.00, vat_rate_code=10)
-        Product.objects.create(name="Test Product 2", price=20.00, vat_rate_code=20)
-        return super().setUp()
+    def tearDown(self) -> None:
+        Product.objects.all().delete()
+        return super().tearDown()
 
     def test_products_page(self):
+        Product.objects.create(name="Test Product 1", price=10.00, vat_rate_code=10)
+        Product.objects.create(name="Test Product 2", price=20.00, vat_rate_code=20)
+
         response = self.client.get("/products/")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "products/index.html")
@@ -19,7 +21,10 @@ class TestProducts(TestCase):
         self.assertContains(response, "Test Product 2")
 
     def test_product_detail_page(self):
-        response = self.client.get("/products/1/")
+        product = Product.objects.create(
+            name="Test Product 1", price=10.00, vat_rate_code=10
+        )
+        response = self.client.get(f"/products/{product.id}/")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "products/detail.html")
         self.assertContains(response, "<h1>Test Product 1</h1>")
--- django_application/products/urls.py
+++ django_application/products/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from . import views
 
 app_name = "products"
-urlpatterns = [path("", views.IndexView.as_view(), name="index")]
+urlpatterns = [
+    path("", views.IndexView.as_view(), name="index"),
+    path("<str:pk>/", views.DetailView.as_view(), name="detail"),
+]
