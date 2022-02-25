888aa094d3a5587a0cd32b1dfb86731dbdcd3be4
climbus <climbus@gmail.com>
2022-02-25 21:26:39 +0100

    command raise exception when is not registered

--- infra/command_bus.py
+++ infra/command_bus.py
@@ -2,6 +2,10 @@ from abc import ABC
 from typing import Protocol
 
 
+class NoRegisteredHandlerException(Exception):
+    pass
+
+
 class Command(ABC):
     pass
 
@@ -16,7 +20,12 @@ class CommandBus:
         self.handlers = {}
 
     def call(self, command):
+        if command.__class__ not in self.handlers:
+            raise NoRegisteredHandlerException()
         self.handlers[command.__class__].handle(command)
 
     def register(self, command: Command, handler: CommandHandler):
         self.handlers[command] = handler
+
+    def clear_registry(self):
+        self.handlers = {}
--- infra/tests/command_bus_test.py
+++ infra/tests/command_bus_test.py
@@ -1,11 +1,18 @@
+import pytest
+
 from unittest.mock import Mock
 
 from infra import command_bus
-from infra.command_bus import Command
+from infra.command_bus import Command, NoRegisteredHandlerException
 
 
 class DummyCommand(Command):
     pass
+
+
+@pytest.fixture(autouse=True)
+def clear_registry():
+    command_bus.clear_registry()
 
 
 def test_should_call_registered_handler():
@@ -16,3 +23,10 @@ def test_should_call_registered_handler():
     command_bus.call(command)
 
     handler.handle.assert_called_once_with(command)
+
+
+def test_should_raise_exception_if_no_handler_registered():
+    command = DummyCommand()
+
+    with pytest.raises(NoRegisteredHandlerException):
+        command_bus.call(command)
