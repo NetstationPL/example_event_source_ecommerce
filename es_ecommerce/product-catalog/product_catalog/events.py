56682aec40e6d876b573db64ffdfe7bb5ede5ff5
climbus <climbus@gmail.com>
2022-02-26 12:54:55 +0100

    intrduced event class

--- es_ecommerce/product-catalog/product_catalog/events.py
+++ es_ecommerce/product-catalog/product_catalog/events.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 from uuid import UUID
 
+from infra.event_store import Event
+
 
 @dataclass
-class ProductRegistered:
+class ProductRegistered(Event):
     name: str
     product_id: UUID
