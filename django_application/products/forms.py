6195289e0ad897228515ba75029846adf0724615
climbus <climbus@gmail.com>
2022-03-05 11:03:27 +0100

    decoupled form from model

--- django_application/products/forms.py
+++ django_application/products/forms.py
@@ -1,13 +1,19 @@
 import uuid
 
 from django import forms
+from taxes import conf
 
-from .models import Product
+
+class VatRateField(forms.ChoiceField):
+    def __init__(self, *args, **kwargs):
+        choices = conf.available_vat_rates
+        choices.insert(0, ("", "---"))
+        kwargs["choices"] = choices
+        super().__init__(*args, **kwargs)
 
 
-class ProductForm(forms.ModelForm):
+class ProductForm(forms.Form):
     product_id = forms.UUIDField(widget=forms.HiddenInput(), initial=uuid.uuid1)
-
-    class Meta:
-        model = Product
-        fields = ("name", "price", "vat_rate")
+    name = forms.CharField(label="Name", max_length=100)
+    price = forms.DecimalField(label="Price", max_digits=10, decimal_places=2)
+    vat_rate = VatRateField(label="VAT Rate", required=False)
--- django_application/products/models.py
+++ django_application/products/models.py
@@ -10,9 +10,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2, blank=True, null=True)
     stock_level = models.IntegerField(default=0, null=True, blank=True)
     registered_at = models.DateTimeField(auto_now_add=True)
-    vat_rate = models.IntegerField(
-        choices=settings.AVAILABLE_VAT_RATES, null=True, blank=True
-    )
+    vat_rate = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return self.name
