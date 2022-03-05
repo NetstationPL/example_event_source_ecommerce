383e124270c4fd49fd331951ee4a57d81283d55b
climbus <climbus@gmail.com>
2022-03-05 11:42:18 +0100

    decimal in vat rate

--- ecommerce/taxes/tests/test_taxes.py
+++ ecommerce/taxes/tests/test_taxes.py
@@ -1,9 +1,10 @@
+from decimal import Decimal
 from unittest.mock import MagicMock
 from uuid import uuid1
 
 from taxes.commands import SetVatRate
 from taxes.services import SetVatRateHandler
-from taxes.taxes.events import VatRateSet
+from taxes.events import VatRateSet
 
 from infra.types import VatRate
 
@@ -12,10 +13,10 @@ def test_set_vat_rate_should_emit_event():
     event_store = MagicMock()
 
     uid = uuid1()
-    command = SetVatRate(product_id=uid, vat_rate=VatRate("10", 10))
+    command = SetVatRate(product_id=uid, vat_rate=VatRate("10", Decimal(10)))
 
     SetVatRateHandler(event_store).handle(command)
 
     event_store.publish.assert_called_once_with(
-        VatRateSet(product_id=uid, vat_rate=VatRate("10", 10)), f"Taxes#{uid}"
+        VatRateSet(product_id=uid, vat_rate=VatRate("10", Decimal(10))), f"Taxes#{uid}"
     )
