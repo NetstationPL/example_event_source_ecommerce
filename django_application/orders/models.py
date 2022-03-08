3d320f3e23df0bf0d65ec16e6e49e5fad287b5e6
climbus <climbus@gmail.com>
2022-03-08 11:16:55 +0100

    order lines model

--- django_application/orders/models.py
+++ django_application/orders/models.py
@@ -3,3 +3,10 @@ from django.db import models
 
 class Order(models.Model):
     pass
+
+
+class OrderLine(models.Model):
+    order_id = models.UUIDField()
+    product_id = models.UUIDField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
