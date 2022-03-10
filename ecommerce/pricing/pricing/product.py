df1405def50c20783526e25e70ce657cf97a8547
climbus <climbus@gmail.com>
2022-03-10 15:38:43 +0100

    added type hints

--- ecommerce/pricing/pricing/product.py
+++ ecommerce/pricing/pricing/product.py
@@ -7,12 +7,12 @@ from infra.types import Money
 class Product(AggregateRoot):
     price: Money
 
-    def set_price(self, price):
+    def set_price(self, price: Money) -> None:
         event = PriceSet(self.uid, price=price)
         self.apply(event)
 
-    def apply_price_set(self, event):
+    def apply_price_set(self, event: PriceSet) -> None:
         self.price = event.price
 
-    def stream_name(self):
+    def stream_name(self) -> str:
         return f"Pricing#{self.uid}"
